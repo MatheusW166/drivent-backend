29db3aba4c365c7c6d49a391ea2604aa9913dd08
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-07-18 17:05:45 -0400

    fix: duplicated export in routers/index.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const port = +process.env.PORT || 4000;
 init().then(() => {
   app.listen(port, () => {
     /* eslint-disable-next-line no-console */
-    console.log(`Server is listening on port ${port}.`);
+    console.log(`🚀 Server is listening on port ${port}.`);
   });
 });
